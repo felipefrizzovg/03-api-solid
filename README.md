460bb943232e079dd76880a9f8424c5cb559d26a
Felipe Frizzo <felipefrizzovg@gmail.com>
2024-11-14 17:49:49 -0300

    Validando data do check-in

--- README.md
+++ README.md
@@ -5,20 +5,20 @@ GymPass style app
 ## Rfs (Requisitos funcionais)
 
 - [X] Deve ser possível se cadastrar
-- [] Deve ser possível se autenticar
-- [] Deve ser possível obter os perfil de um usuário logado
+- [X] Deve ser possível se autenticar
+- [X] Deve ser possível obter os perfil de um usuário logado
 - [] Deve ser possível obter o número de check-ins realizados pelo usuário logado
 - [] Deve ser possível obter o usuário logado obter seu histórico de check-ins
 - [] Deve ser possível obter o usuário logado buscar academias próximas
 - [] Deve ser possível obter o usuário logado buscar academias pelo nome
-- [] Deve ser possível obter o usuário logado realizar check-in em uma academia
+- [X] Deve ser possível obter o usuário logado realizar check-in em uma academia
 - [] Deve ser possível validar o check-in de um usuário
 - [] Deve ser possível cadastrar uma academia
 
 ## Rn (Regras de negócio)
 
 - [X] O usuário não deve poder se cadastrar com um e-mail duplicado
-- [] O usuário não pode fazer dois check-ins no mesmo dia
+- [X] O usuário não pode fazer dois check-ins no mesmo dia
 - [] O usuário não pode fazer check-in se não estiver perto (100m) da academia
 - [] O check-in só pode ser validado até 20min após criado
 - [] O check-in só pode ser validado por administradores
@@ -27,6 +27,6 @@ GymPass style app
 ## RNFs (Requisitos não-funcionais)
 
 - [X] A senha do usuário precisa estar criptografada
-- [] Os dados da aplicação precisam estar persistidos em um banco PostgreSQL
+- [X] Os dados da aplicação precisam estar persistidos em um banco PostgreSQL
 - [] Todas listas de dados precisam estar paginadas com 20 items por página
 - [] O usuário deve ser identificado com um JWT (JSON Web Token)
--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,14 +1,27 @@
 import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from './check-in'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { Decimal } from '@prisma/client/runtime/library'
 
 let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
 let sut: CheckInUseCase
 
 describe('Check-in Use Case', () => {
   beforeEach(() => {
-    checkInsRepository = new InMemoryCheckInsRepository
-    sut = new CheckInUseCase(checkInsRepository)
+    checkInsRepository = new InMemoryCheckInsRepository()
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+    gymsRepository.items.push({
+      id: 'gym-01',
+      title: 'Javascript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(0),
+      longitude: new Decimal(0)
+    })
 
     vi.useFakeTimers()
   })
@@ -21,6 +34,8 @@ describe('Check-in Use Case', () => {
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
+      userLatitude: -23.4781784,
+      userLongitude: -46.6801928
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
@@ -32,12 +47,16 @@ describe('Check-in Use Case', () => {
     await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
+      userLatitude: -23.4781784,
+      userLongitude: -46.6801928
     })
 
     expect(() => 
       sut.execute({
         gymId: 'gym-01',
         userId: 'user-01',
+        userLatitude: -23.4781784,
+        userLongitude: -46.6801928
       })
     ).rejects.toBeInstanceOf(Error)
   })
@@ -48,13 +67,17 @@ describe('Check-in Use Case', () => {
     await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
+      userLatitude: -23.4781784,
+      userLongitude: -46.6801928
     })
 
-    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
 
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
+      userLatitude: -23.4781784,
+      userLongitude: -46.6801928
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
