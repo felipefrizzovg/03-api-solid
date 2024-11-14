9a50410b974f8c4e65b9ee8fa770ecd197b82ddd
Felipe Frizzo <felipefrizzovg@gmail.com>
2024-11-14 17:30:23 -0300

    TDD & Mocking

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -14,6 +14,12 @@ export class CheckInUseCase {
   constructor(private checkInsRepository: CheckInsRepository) {}
 
   async execute({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(userId, new Date());
+
+    if (checkInOnSameDate) {
+      throw new Error()
+    }
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId
